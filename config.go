afa9d2b541b22a1eb22229af43ef4ced85a8f25f
Singee <imsingee@gmail.com>
2020-03-01 21:52:11 +0800

    add environment value as a config source

--- config.go
+++ config.go
@@ -21,17 +21,16 @@ func loadConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			// Config file not found; ignore
-			log.Fatal("Cannot find config file.")
+			log.Println("Cannot find config file.")
 		} else {
 			log.Fatal("Can not open or parse config.yml: ", err)
 		}
 	}
-}
 
-func saveConfig() {
-	err := viper.WriteConfig()
-	if err != nil {
-		log.Println("Cannot refresh config")
-	}
-
+	_ = viper.BindEnv("token_v2", "NOTION_TOKEN")
+	_ = viper.BindEnv("converter.force", "NOTION_CONVERTER_FORCE")
+	_ = viper.BindEnv("render.checkbox", "NOTION_RENDER_CHECKBOX")
+	_ = viper.BindEnv("database.post", "NOTION_DATABASE_POST")
+	_ = viper.BindEnv("user.locale", "NOTION_USER_LOCALE")
+	_ = viper.BindEnv("user.timezone", "NOTION_USER_TIMEZONE")
 }
